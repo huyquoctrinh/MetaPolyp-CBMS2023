a5b782a1e5ae821ae14b228b33ce09e6fb749ab9
Trinh Quoc Huy <47996835+huyquoctrinh@users.noreply.github.com>
2023-07-12 00:14:12 +0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -9,17 +9,25 @@ def load_model(model_path):
 
 def predict_single(model, imgPath):
     img = cv2.imread(imgPath)
-    img /= 255
+    img = cv2.resize(img, (256, 256))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = np.expand_dims(img, axis = 0)
+    # img = img / 255.
     result = model.predict(img)
     return result
 
+def mask_parse(mask):
+    mask = np.squeeze(mask)
+    mask = [mask, mask, mask]
+    mask = np.transpose(mask, (1, 2, 0))
+    return mask*255
+
 if __name__ == "__main__":
     save_path = "best_model.h5"
-    img_in = "test.png"
-    img_out = "out.png"
+    img_in = "cju0qoxqj9q6s0835b43399p4.jpg"
+    img_out = "out1.jpg"
 
     model = load_model(save_path)
-    
-    mask = predict_single(model, img_out)
-    mask_out = np.dstack([mask, mask, mask])
-    cv2.imwrite(img_out, mask_out)
+    masks = predict_single(model, img_in)
+    out = mask_parse(masks)
+    cv2.imwrite(img_out, out)
